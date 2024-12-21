dd9fbb31d2b5b5b70272f5f5d5ede90f2877647b
Angus.Young <angusyoung@qq.com>
2024-12-21 22:16:34 +0800

    Update
    * 更新导出

--- src/components.ts
+++ src/components.ts
@@ -3,31 +3,43 @@ export { default as VbAnimateNum } from './packages/animateNumber';
 // 头像
 export { default as VbAvatar } from './packages/avatar';
 // 返回
-export { TLBack as VbTLBack } from './packages/back'
-export { default as VbBack } from './packages/back';
+export {
+	default as VbBack,
+	TLBack as VbTLBack
+} from './packages/back'
 // 级联选择器
 export { default as VbCascade } from './packages/cascade';
 // 图表
-export { default as VbCharts } from './packages/charts';
-export { Bar as VbChartsBar } from './packages/charts';
-export { Gauge as VbChartsGauge } from './packages/charts';
-export { Line as VbChartsLine } from './packages/charts';
-export { Pie as VbChartsPie } from './packages/charts';
-export { Radar as VbChartsRadar } from './packages/charts';
+export {
+	default as VbCharts,
+	Bar as VbChartsBar,
+	Gauge as VbChartsGauge,
+	Line as VbChartsLine,
+	Pie as VbChartsPie,
+	Radar as VbChartsRadar
+} from './packages/charts';
 // 选择项
 export { default as VbChoose } from './packages/choose';
 // 折叠面板
-export { default as VbCollapse } from './packages/collapse';
-export { CollapsePanel as VbCollapsePanel } from './packages/collapse';
+export {
+	default as VbCollapse,
+	CollapsePanel as VbCollapsePanel
+} from './packages/collapse';
 // 倒计时
-export { default as VbCountdown } from './packages/countdown';
-export { CountdownBar as VbCountdownBar } from './packages/countdown';
+export {
+	default as VbCountdown,
+	CountdownBar as VbCountdownBar
+} from './packages/countdown';
 // 内容展示容器
-export { default as VbDataView } from './packages/dataView';
-export { DataViewItem as VbDataViewItem } from './packages/dataView';
+export {
+	default as VbDataView,
+	DataViewItem as VbDataViewItem
+} from './packages/dataView';
 // 日期选择器
-export { default as VbDatetime } from './packages/datetimePicker';
-export { TimePicker as VbTime } from './packages/datetimePicker';
+export {
+	default as VbDatetime,
+	TimePicker as VbTime
+} from './packages/datetimePicker';
 // 对话容器
 export { default as VbDialog } from './packages/dialog';
 // 弹出菜单
@@ -37,45 +49,57 @@ export { default as VbDualInput } from './packages/dualInput';
 // 空白状态
 export { default as VbEmpty } from './packages/empty';
 // 表单容器
-export { default as VbForm } from './packages/form';
-// 表单项容器
-export { FormElement as VbFormItem } from './packages/form';
+export {
+	default as VbForm,
+	// 表单项容器
+	FormElement as VbFormItem
+} from './packages/form';
 // 画廊
 export { default as VbGallery } from './packages/gallery';
 // 自增数字框
 export { default as VbIncrement } from './packages/increment';
 // 输入框
-export { default as VbInput } from './packages/input';
-// 密文框
-export { PasswordInput as VbPassword } from './packages/input';
+export {
+	default as VbInput,
+	// 密文框
+	PasswordInput as VbPassword
+} from './packages/input';
 // 交互追踪
 export { default as VbInteractiveTracker } from './packages/InteractiveTracker';
 // 加载动画
-export { default as VbLoading } from './packages/loading';
-export { AnimateBall as VbLoadingBall } from './packages/loading';
-export { ProgressBar as VbLoadingBar } from './packages/loading';
-export { AnimateDot as VbLoadingDot } from './packages/loading';
-export { AnimateWater as VbLoadingWater } from './packages/loading';
+export {
+	default as VbLoading,
+	AnimateBall as VbLoadingBall,
+	ProgressBar as VbLoadingBar,
+	AnimateDot as VbLoadingDot,
+	AnimateWater as VbLoadingWater
+} from './packages/loading';
 // 菜单
-export { default as VbMenu } from './packages/menu';
-export { Link as VbLink } from './packages/menu';
+export {
+	default as VbMenu,
+	Link as VbLink
+} from './packages/menu';
 // 弹框容器
 export { default as VbModal } from './packages/modal';
-// notify
-export { Notify as VbNotify } from './packages/messages';
-// Popup 弹窗
-export { Popup as VbPopup } from './packages/messages';
-// Toast
-export { Toast as VbToast } from './packages/messages';
 // 通知统一组件
-export { Tell as VbTell } from './packages/messages';
+export {
+	Tell as VbTell,
+	// Notify
+	Notify as VbNotify,
+	// Popup 弹窗
+	Popup as VbPopup,
+	// Toast
+	Toast as VbToast
+} from './packages/messages';
 // 分页
 export { default as VbPagination } from './packages/pagination';
 // 评级
 export { default as VbRating } from './packages/rating';
 // 下拉选择器
-export { default as VbSelect } from './packages/select';
-export { SelectorNative as VbSelectNative } from './packages/select';
+export {
+	default as VbSelect,
+	SelectorNative as VbSelectNative
+} from './packages/select';
 // 侧边页容器
 export { default as VbSidePage } from './packages/sidePage';
 // 排序状态按钮
@@ -96,11 +120,13 @@ export { default as VbTags } from './packages/tags';
 export { default as VbTips } from './packages/tooltips';
 // 树
 export { default as VbTree } from './packages/tree';
-// 文件预览
-export { Preview as VbPreview } from './packages/uploader';
-// 全能上传插件
-export { SpecialImageUpload as VbUPluginSI } from './packages/uploader';
-// 裁剪图片上传插件
-export { CropImageUpload as VbUPluginCI } from './packages/uploader';
 // 上传组件
-export { default as VbUploader } from './packages/uploader';
+export {
+	default as VbUploader,
+	// 文件预览
+	Preview as VbPreview,
+	// 全能上传插件
+	SpecialImageUpload as VbUPluginSI,
+	// 裁剪图片上传插件
+	CropImageUpload as VbUPluginCI
+} from './packages/uploader';
