1b0d45998e8a2df374247f404ed6f45cad24603c
Angus.Young <angusyoung@qq.com>
2025-05-01 16:10:11 +0800

    fix(animateNumber): 优化动画效果和文本处理
    - 添加 isAnimate 控制动画开关
    - 调整动画参数，提高动画流畅度
    - 修复数字滚动时的文字排版问题

--- src/packages/animateNumber/src/AnimateNum.vue
+++ src/packages/animateNumber/src/AnimateNum.vue
@@ -8,10 +8,19 @@ const props = defineProps({
 	}
 });
 const charList = computed(() => {
-	return props.text?.split('') ?? [];
+	return props.text?.toString().split('') ?? [];
 });
+const isAnimate = ref(true);
 const oldPoints = ref<string[]>([]);
-watch(charList, (newList, oldList) => {
+watch(isAnimate, (ani) => {
+	if (ani) {
+		setTimeout(() => {
+			isAnimate.value = false;
+		}, 610);
+	}
+}, {immediate: true});
+watch(charList, (newList, oldList, onCleanup) => {
+	isAnimate.value = true;
 	oldPoints.value = oldList;
 	// 如果数组长度不一致，在前面补齐数组长度
 	if (newList.length > oldList.length) {
@@ -31,9 +40,12 @@ watch(charList, (newList, oldList) => {
 			<span v-if="isNaN(parseInt(char, 0))">{{ char }}</span>
 			<span
 					class="number-char"
-					:style="`--ani-init: -${Number(oldPoints[index]) || 0};
-					--ani-point: -${char};
-					--ani-delay: ${charList.length - index}`"
+					:class="{'is-animation': isAnimate}"
+					:style="isAnimate ? `
+						--ani-init: -${Number(oldPoints[index]) || 0};
+						--ani-point: -${char};
+						--ani-delay: ${charList.length - index};
+					` : null"
 					v-else>
 				{{ char }}
 			</span>
@@ -78,24 +90,27 @@ watch(charList, (newList, oldList) => {
 	}
 
 	.number-char {
-		--time-duration: .8s;
-		--time-delay: calc(var(--ani-delay) * .1s);
-		--end-delay: calc(var(--time-delay) + var(--time-duration));
-
 		position: relative;
-		animation: charShow 0s ease-in-out var(--end-delay) both;
 		min-width: 1ch;
 
-		&::after {
-			content: '0123456789';
-			position: absolute;
-			display: block;
-			word-break: break-all;
-			font-weight: inherit;
-			text-indent: initial;
-			top: 0;
-			left: 0;
-			animation: scrollNum var(--time-duration) steps(10) var(--time-delay) both, fadeOut 0s var(--end-delay) both;
+		&.is-animation {
+			--time-duration: .6s;
+			--time-delay: calc(var(--ani-delay) * .1s);
+			--end-delay: calc(var(--time-delay) + var(--time-duration));
+			animation: charShow 0s ease-in-out var(--end-delay) both;
+
+			&::after {
+				content: '0123456789';
+				position: absolute;
+				display: block;
+				word-break: break-all;
+				white-space: break-spaces;
+				font-weight: inherit;
+				text-indent: initial;
+				top: 0;
+				left: 0;
+				animation: scrollNum var(--time-duration) steps(10) var(--time-delay) both;
+			}
 		}
 	}
 }
