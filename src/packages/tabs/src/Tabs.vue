03ee226760476edac4fc4b188784642be8bfc318
Angus.Young <angusyoung@qq.com>
2026-09-10 17:52:45 +0800

    feat(tabs): 重构标签页组件并新增置底样式支持

--- src/packages/tabs/src/Tabs.vue
+++ src/packages/tabs/src/Tabs.vue
@@ -1,44 +1,74 @@
 <script setup lang="ts">
-import { ref, watchEffect } from 'vue';
+import { ref, watch } from 'vue';
 
+defineOptions({inheritAttrs: false});
 const props = defineProps({
 	currentIndex: {
-		type   : Number,
-		default: 0
+		type: Number
 	},
 	list        : {
 		type   : Array,
-		default: []
+		default: () => []
 	}
 });
 const emit = defineEmits(['change']);
 const activeIndex = ref(0);
 
-watchEffect(() => {
-	if (typeof props.currentIndex !== 'undefined') {
-		setTimeout(() => {
-			changeTab(props.currentIndex);
-		});
+watch(() => props.currentIndex, (current) => {
+	if (typeof current !== 'undefined') {
+		changeTab(current);
 	}
-});
+}, {immediate: true});
 
 function changeTab(index: number) {
+	if (index === activeIndex.value) {
+		return;
+	}
 	activeIndex.value = index;
 	emit('change', index);
 }
 </script>
 
 <template>
-	<div class="tabs" v-bind="$attrs">
-		<ul>
-			<slot :current="activeIndex" :changeTab="changeTab">
-				<li :class="{'is-active': activeIndex === index}" :key="item as string" v-for="(item, index) in list">
-					<a @click="changeTab(index)">{{ item }}</a>
-				</li>
-			</slot>
-		</ul>
+	<div class="vb-tabs">
+		<div class="tabs" v-bind="$attrs">
+			<ul>
+				<slot :current="activeIndex" :changeTab="changeTab">
+					<li :class="{'is-active': activeIndex === index}" :key="item as string" v-for="(item, index) in list">
+						<a @click="changeTab(index)"><span>{{ item }}</span></a>
+					</li>
+				</slot>
+			</ul>
+		</div>
+		<div class="tabs-content" v-if="Object.keys($slots).filter((n:string) => n !== 'default').length">
+			<template :key="index" v-for="(_, index) in list">
+				<slot :name="`item${index}`" v-if="$slots[`item${index}`] && activeIndex === index"/>
+			</template>
+		</div>
 	</div>
-	<template :key="index" v-for="(_, index) in list">
-		<slot :name="`item${index}`" v-if="$slots[`item${index}`] && activeIndex === index"/>
-	</template>
 </template>
+
+<style lang="scss" scoped>
+.vb-tabs {
+	display: flex;
+	flex-direction: column;
+
+	.tabs {
+		&.is-bottom {
+			order: 2;
+			margin-bottom: 0;
+			transform: scaleY(-1);
+
+			ul li a span {
+				display: inline-block;
+				transform: scaleY(-1);
+			}
+
+			& + .tabs-content {
+				order: 1;
+				margin-bottom: var(--bulma-block-spacing);
+			}
+		}
+	}
+}
+</style>
